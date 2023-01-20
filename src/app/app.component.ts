01357b6fe5dd8eae48dfedd93f82c9c63c5c9097
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-01-20 14:43:04 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  imports: [MatSnackBarModule]
 })
 export class AppComponent {
-  title = 'snack-bar';
+  constructor(public _snackBar: MatSnackBar) { }
+
+  openSnackBar(message: string,
+    duration: number = 5000,
+    appearance: 'fill' | 'outline' | 'soft' = 'fill',
+    type: 'info' | 'success' | 'error' | 'alert' = 'info'): void {
+
+    const config: MatSnackBarConfig = {
+      duration: duration,
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+      panelClass: [`snackbar-type-${appearance}-${type}`]
+    };
+    this._snackBar.open(message, 'Ok', config);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations'
 
-import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations()
+  ]
+})
   .catch(err => console.error(err));
